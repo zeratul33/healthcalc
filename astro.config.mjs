3e94458f25da38e4f78fe5f8195c9f26f4d0816c
zeratul33 <zeratul_he@hotmail.com>
2026-08-08 22:23:22 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'https://healthcalc.ciciad.tech',
   output: 'static',
-  integrations: [react(), tailwind()],
+  integrations: [react(), tailwind(), sitemap()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'zh', 'es', 'fr', 'de', 'ja', 'ko', 'pt', 'ru', 'ar'],
